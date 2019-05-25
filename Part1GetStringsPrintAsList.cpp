d303024037ff95bd2f30f19da87824bfe4e133ad
Sepideh Abadpour <abadpours@gmail.com>
2019-05-25 18:35:52 +0430

    Add Part1GetStringsPrintAsList.cpp file

---
+++ Part1GetStringsPrintAsList.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string current;
+	int counter = 1;
+	while (cin >> current)
+	{
+		if (counter == 1)
+			cout << '[' << current << ", ";
+		else
+			cout << current << ", ";
+		counter = counter + 1;
+	}
+	return 0;
+}
